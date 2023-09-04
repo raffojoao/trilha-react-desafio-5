843cf08b1abafe82e32f97f19809f6fd9727a2bd
joao.raffo <raffo.joao@gmail.com>
2023-09-04 15:52:50 -0300

    initial commit

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://xdlrwtzpzpdfwgowwuur.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbHJ3dHpwenBkZndnb3d3dXVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MzgwMTIsImV4cCI6MjAwOTQxNDAxMn0.Adsf_bhbHF52tqVJmH6nKsvF6L1EepXDbvgIM8hmcRg",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbHJ3dHpwenBkZndnb3d3dXVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MzgwMTIsImV4cCI6MjAwOTQxNDAxMn0.Adsf_bhbHF52tqVJmH6nKsvF6L1EepXDbvgIM8hmcRg"
     }
 })
